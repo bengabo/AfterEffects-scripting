08bdd15e3452b6d60600368ea64243b054039f38
bengabo <ben@bengabo.com>
2020-05-17 17:17:13 +0200

    loop created with different compNames, layerNames and titles

--- AE-script_01.jsx
+++ AE-script_01.jsx
@@ -1,6 +1,7 @@
-function createComp (compName, layerName, valueX, valueY) {
+function createComp (compName, layerName, title, valueX, valueY) {
   const newComp = app.project.items.addComp(compName, 1920, 1080, 1, 5, 25);
-  const newLayer = newComp.layers.addText(layerName);
+  const newLayer = newComp.layers.addText(title);
+  newLayer.name = layerName;
   newLayer.position.setValue([valueX, valueY]);
   newLayer.position.expression = "wiggle(2, 25)";
   newLayer.opacity.addKey(0);
@@ -9,8 +10,15 @@ function createComp (compName, layerName, valueX, valueY) {
   newLayer.opacity.setValueAtKey(2, 100);
 }
 
-createComp("Bengabo", "Text1", 200, 1000);
+app.beginUndoGroup("code");
+const compNames = ["Comp1", "Comp2", "Comp3"];
+const layerNames = ["text1", "text2", "text3"];
+const titles = ["Bengabo", "Wamwam", "Chateau"];
 
+for (i=0; i<compNames.length; i++) {
+  createComp(compNames[i], layerNames[i], titles[i], 200, 1000);
+}
+app.endUndoGroup;
 
 // var newComp = app.project.items.addComp("Wamwam", 1920, 1080, 1, 25, 200);
 // var newLayer = newComp.layers.addText("Bengabo");
